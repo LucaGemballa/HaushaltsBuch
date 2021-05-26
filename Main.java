5cca09a56f1828e6c6c0c179cbfd67b5e85ecca4
LSG2001 <luca.gemballa@gmx.de>
2021-05-26 12:12:30 +0200

    BaseRunning

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class haushaltMain (){
-
-    public static void main (String args[]){
-        System.out.println("hallo");
-    }
-}
---
+++ buchPackage/src/reportedActions/moneyAction.java
@@ -0,0 +1,4 @@
+package reportedActions;
+
+public class moneyAction {
+}
